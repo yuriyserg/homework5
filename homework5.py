017066eaa988cefa9c3e26e2702f6f351b73fc16
yuriyserg <pavlovys@gmail.com>
2024-07-20 15:45:45 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, True, 'string')
+print(immutable_var)     #immutable_var [1] = 'water' - кортеж неизменяемая коллекция.
+mutable_list = ['june', 'july', 'august']
+print(mutable_list)
+mutable_list.append('september')
+print(mutable_list)
